daaaf887aba4e4f73cd190fdf0897514e6ebb7c3
love <hoangtien96da@gmail.com>
2023-02-28 11:08:09 +0700

    them

---
+++ Circle.php
@@ -0,0 +1,14 @@
+<?php
+include_once ('Shape.php');
+class Circle extends Shape
+{
+    public $radius;
+    public function __construct($name,$radius){
+        parent ::__construct($name);
+        $this->radius = $radius;
+    }
+    public function calculateArea(){
+        return pi() * $this->radius *2;
+    }
+}
+?>
